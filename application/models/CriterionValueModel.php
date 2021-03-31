da90fb148a391fbf2c994ccf58dae3909e85d416
dodinovembri <dodinovembri32gmail.com>
2021-03-31 16:58:17 +0700

    update

---
+++ application/models/CriterionValueModel.php
@@ -0,0 +1,34 @@
+<?php
+
+class CriterionValueModel extends CI_Model
+{
+    private $_table = "criterion_value";
+
+    public function get()
+    {
+    	return $this->db->get($this->_table);
+    }
+
+    public function insert($data)
+    {
+        return $this->db->insert($this->_table, $data);
+    }
+
+    public function getById($id)
+    {
+        $this->db->where('id', $id);
+        return $this->db->get($this->_table);
+    }   
+
+    public function update($data, $id)
+    {
+        $this->db->where('id', $id);
+        return $this->db->update($this->_table, $data);
+    }    
+
+    public function destroy($id)
+    {
+        $this->db->where('id', $id);
+        return $this->db->delete($this->_table);
+    }            
+}
