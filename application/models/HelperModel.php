8879801ad45ca34d43b5646396a2da10e096570c
dodinovembri <dodinovembri32@gmail.com>
2021-06-04 23:16:38 +0700

    update

--- application/models/HelperModel.php
+++ application/models/HelperModel.php
@@ -14,6 +14,6 @@ class HelperModel extends CI_Model
 
     public function getWithJoin()
     {
-        return $this->db->query("SELECT rating.*, criterion_value.information AS information, criterion_value.score AS score, criteria.criteria_code AS criteria_code FROM rating JOIN criterion_value ON rating.criterion_value_id = criterion_value.id JOIN employee ON rating.employee_id = employee.id JOIN criteria ON rating.criteria_id = criteria.id WHERE employee.type = 4");
+        return $this->db->query("SELECT rating.*, criterion_value.information AS information, criterion_value.score AS score, criteria.criteria_code AS criteria_code FROM rating JOIN criterion_value ON rating.criterion_value_id = criterion_value.id JOIN employee ON rating.employee_id = employee.id JOIN criteria ON rating.criteria_id = criteria.id WHERE employee.type = 4 ORDER by employee_id, criteria_id");
     }
 }
